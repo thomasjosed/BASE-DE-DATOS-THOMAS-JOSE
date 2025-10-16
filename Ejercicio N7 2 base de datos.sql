a2999086cecbc42e671b86c8cd143152c3f268f4
thomasjosed <thomasjosedaruich@gmail.com>
2025-10-16 02:27:46 -0300

    Add files via upload

---
+++ Ejercicio N7 2 base de datos.sql
@@ -0,0 +1,27 @@
+drop table if exists peliculas;
+
+ create table peliculas(
+  titulo varchar(20),
+  actor varchar(20),
+  duracion integer,
+  cantidad integer
+ );
+
+ insert into peliculas (titulo, actor, duracion, cantidad)
+  values ('Mision imposible','Tom Cruise',120,3);
+ insert into peliculas (titulo, actor, duracion, cantidad)
+  values ('Mision imposible 2','Tom Cruise',180,4);
+ insert into peliculas (titulo, actor, duracion, cantidad)
+  values ('Mujer bonita','Julia R.',90,1);
+ insert into peliculas (titulo, actor, duracion, cantidad)
+  values ('Elsa y Fred','China Zorrilla',80,2);
+
+ select * from peliculas
+  where duracion<=90;
+
+ select * from peliculas
+  where actor<>'Tom Cruise';
+
+ select titulo,actor,cantidad
+  from peliculas
+  where cantidad >2;
