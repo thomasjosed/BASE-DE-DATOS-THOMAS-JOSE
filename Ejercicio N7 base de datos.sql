9659a9bd563c460b860755c3ad716e3814c37713
thomasjosed <thomasjosedaruich@gmail.com>
2025-10-16 02:26:08 -0300

    Add files via upload

---
+++ Ejercicio N7 base de datos.sql
@@ -0,0 +1,34 @@
+drop table if exists articulos;
+
+ create table articulos(
+  codigo integer,
+  nombre varchar(20),
+  descripcion varchar(30),
+  precio float,
+  cantidad integer
+ );
+
+ insert into articulos (codigo, nombre, descripcion, precio,cantidad)
+  values (1,'impresora','Epson Stylus C45',400.80,20);
+ insert into articulos (codigo, nombre, descripcion, precio,cantidad)
+  values (2,'impresora','Epson Stylus C85',500,30);
+ insert into articulos (codigo, nombre, descripcion, precio,cantidad)
+  values (3,'monitor','Samsung 14',800,10);
+ insert into articulos (codigo, nombre, descripcion, precio,cantidad)
+  values (4,'teclado','ingles Biswal',100,50);
+ insert into articulos (codigo, nombre, descripcion, precio,cantidad)
+  values (5,'teclado','español Biswal',90,50);
+
+ select * from articulos
+  where nombre='impresora';
+
+ select * from articulos
+  where precio>=400;
+
+ select codigo,nombre
+  from articulos
+  where cantidad<30;
+
+ select nombre, descripcion
+  from articulos
+  where precio<>100;
